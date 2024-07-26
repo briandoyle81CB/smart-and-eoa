1b28cc25a85158408b2275fd42c95125871fce75
Brian Doyle <brian.doyle@coinbase.com>
2024-07-26 17:12:24 -0400

    Broken attempt to use wallet aggregator

--- src/components/OnchainProviders.tsx
+++ src/components/OnchainProviders.tsx
@@ -1,29 +1,65 @@
-'use client';
-import { ReactNode } from 'react';
-import { OnchainKitProvider } from '@coinbase/onchainkit';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { base } from 'viem/chains';
-import { WagmiProvider } from 'wagmi';
-import { NEXT_PUBLIC_CDP_API_KEY } from '../config';
-import { wagmiConfig } from '../wagmi';
+"use client";
+import { ReactNode } from "react";
+import { OnchainKitProvider } from "@coinbase/onchainkit";
+import {
+  RainbowKitProvider,
+  connectorsForWallets,
+} from "@rainbow-me/rainbowkit";
+import {
+  metaMaskWallet,
+  rainbowWallet,
+  coinbaseWallet,
+} from "@rainbow-me/rainbowkit/wallets";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createConfig, http, WagmiProvider } from "wagmi";
+import { baseSepolia } from "wagmi/chains";
 
-type Props = { children: ReactNode };
+import "@coinbase/onchainkit/styles.css";
+import "@rainbow-me/rainbowkit/styles.css";
 
-const SCHEMA_ID =
-  '0xf8b05c79f090979bf4a80270aba232dff11a10d9ca55c4f88de95317970f0de9';
+type Props = {
+  children: ReactNode;
+};
 
 const queryClient = new QueryClient();
+
+const connectors = connectorsForWallets(
+  [
+    {
+      groupName: "Recommended Wallet",
+      wallets: [coinbaseWallet],
+    },
+    {
+      groupName: "Other Wallets",
+      wallets: [rainbowWallet, metaMaskWallet],
+    },
+  ],
+  {
+    appName: "onchainkit",
+    projectId: "51407fcf066d74968d9a1a4c6da0d994",
+  }
+);
+
+const wagmiConfig = createConfig({
+  connectors,
+  chains: [baseSepolia],
+  ssr: true, // If your dApp uses server side rendering (SSR)
+  transports: {
+    [baseSepolia.id]: http(),
+  },
+});
 
 function OnchainProviders({ children }: Props) {
   return (
     <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
         <OnchainKitProvider
-          apiKey={NEXT_PUBLIC_CDP_API_KEY}
-          chain={base}
-          schemaId={SCHEMA_ID}
+          apiKey={process.env.PUBLIC_ONCHAINKIT_API_KEY}
+          chain={baseSepolia}
         >
-          {children}
+          <RainbowKitProvider modalSize="compact">
+            {children}
+          </RainbowKitProvider>
         </OnchainKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
